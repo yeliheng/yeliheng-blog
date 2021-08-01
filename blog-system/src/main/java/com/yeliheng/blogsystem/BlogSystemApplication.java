6bd0274c318f7780e947a794d2b9ad73756436a9
Yeliheng <yeliheng00@163.com>
2021-08-01 17:09:55 +0800

    feat: 初始化项目

---
+++ blog-system/src/main/java/com/yeliheng/blogsystem/BlogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yeliheng.blogsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSystemApplication.class, args);
+    }
+
+}
---
+++ blog-system/src/test/java/com/yeliheng/blogsystem/BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeliheng.blogsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
