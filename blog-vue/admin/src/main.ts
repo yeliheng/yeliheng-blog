fec4a65e718ccc12f82c5c7e1d4ba70487b03660
yeliheng <yeliheng00@163.com>
2021-10-14 17:25:19 +0800

    feat: 添加Element UI

--- blog-vue/admin/src/main.ts
+++ blog-vue/admin/src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import * as Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = Vue.createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(ElementPlus);
+app.mount('#app');
---
+++ blog-vue/admin/src/views/login/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login-container">
+      <div class="login-card">
+          <div class="login-title">管理员登录</div>
+          <!-- 登录表单 -->
+          <el-form :model="form" ref="form" :rules="rules" label-width="80px" :inline="false" size="normal">
+            <el-form-item label="">
+              <el-input></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary">立即创建</el-button>
+              <el-button>取消</el-button>
+            </el-form-item>
+          </el-form>
+          
+        
+          
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
