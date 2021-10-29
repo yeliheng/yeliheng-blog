bec32637c35fe3386ce4a3a6b56a42fb0183bcc7
yeliheng <yeliheng00@163.com>
2021-10-29 20:14:36 +0800

    fix: 修复MenuMapper中拼写错误导致的问题

--- blog-system/src/main/java/com/yeliheng/blogsystem/security/handle/LogoutSuccessHandlerImpl.java
+++ blog-system/src/main/java/com/yeliheng/blogsystem/security/handle/LogoutSuccessHandlerImpl.java
@@ -1,13 +1,11 @@
 package com.yeliheng.blogsystem.security.handle;
 
 import com.alibaba.fastjson.JSON;
-import com.yeliheng.blogsystem.common.CommonResponse;
 import com.yeliheng.blogsystem.dto.ErrorDTO;
 import com.yeliheng.blogsystem.entity.LoginUser;
 import com.yeliheng.blogsystem.utils.ServletUtils;
 import com.yeliheng.blogsystem.utils.StringUtils;
 import com.yeliheng.blogsystem.utils.TokenUtils;
-import com.yeliheng.blogsystem.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
@@ -23,19 +21,19 @@ public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
 
     @Autowired
     private TokenUtils tokenUtils;
-    @Autowired
-    private UserUtils userUtils;
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-        LoginUser loginUser = tokenUtils.getLoginUser(httpServletRequest);
-        if(StringUtils.isNotNull(loginUser))
-            tokenUtils.deleteLoginUser(loginUser.getUser().getId());
-        ErrorDTO errorDTO = new ErrorDTO();
-        errorDTO.setErrCode("");
-        errorDTO.setMessage("登出成功!");
-        errorDTO.setHttpCode(200);
+        try{
+            LoginUser loginUser = tokenUtils.getLoginUser(httpServletRequest);
+            if(StringUtils.isNotNull(loginUser))
+                tokenUtils.deleteLoginUser(loginUser.getUser().getId());
+            ErrorDTO errorDTO = new ErrorDTO();
+            errorDTO.setErrCode("");
+            errorDTO.setMessage("登出成功!");
+            errorDTO.setHttpCode(200);
+            ServletUtils.renderString(httpServletResponse, JSON.toJSONString(errorDTO),200);
+        }catch (Exception ignored) {}
 
-        ServletUtils.renderString(httpServletResponse, JSON.toJSONString(errorDTO),200);
     }
 }
