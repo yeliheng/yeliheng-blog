44afa0eb994805bf677d3b591ffc7a68529153a1
Yeliheng <yeliheng00@163.com>
2021-12-01 14:48:12 +0800

    feat: 初始化博客客户端

---
+++ blog-vue/blog/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+  * {
+    padding: 0;
+    margin: 0;
+    //box-sizing: border-box;
+}
+
+  html {
+    font-size: 14px;
+    font-family: Avenir, Helvetica, Arial, sans-serif, 'Microsoft YaHei';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #666666;
+  }
+  
+  body{
+      background: #1b1b1b;
+      
+  }
+
+  @media screen and (min-width: 1450px) {
+    html {
+        font-size: 75%;
+    }
+}
+</style>
