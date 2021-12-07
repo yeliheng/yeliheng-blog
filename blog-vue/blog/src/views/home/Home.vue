d5cb272cc7db5e3f7040ed87a6fd369bf2197c5e
yeliheng <yeliheng00@163.com>
2021-12-07 16:57:58 +0800

    feat: Header适配手机

--- blog-vue/blog/src/views/home/Home.vue
+++ blog-vue/blog/src/views/home/Home.vue
@@ -1,12 +1,19 @@
 <template>
-
 <div class="home-container">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
+  <div class="header-mobile">
+    <div class="header-bg">
+    </div>
+    <div class="button-container-mobile">
+        <span class="iconfont icon-mulu" style="font-size: 1.4rem;"></span>
+    </div>
+    
+  </div>
     <div class="header">
         <!-- TODO: 后台获取站点信息 -->
         <!-- <div class="title">Yeliheng的技术小站</div> -->
         <iframe src="fontmesh/index.html"></iframe>
-        <!-- <div class="button-container">
+         <div class="button-container">
             <div class="index-button header-button">
                 <span class="iconfont icon-shouye"></span>
                 <span>首页</span>
@@ -29,7 +36,7 @@
                 <span class="iconfont icon-youqinglianjie"></span>
                 <span>友情链接</span>
             </div>
-        </div> -->
+        </div>
     </div>
     <div class="sidebar">
         <div class="my-info"></div>
@@ -48,6 +55,19 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+@keyframes button-anim
+{
+    0%   {margin-top: 2rem;opacity: 0;}
+    100% {margin-top: 7rem;opacity: 1;}
+}
+ 
+@-webkit-keyframes button-anim /* Safari 与 Chrome */
+{
+    0%   {margin-top: 2rem;opacity: 0;}
+    100% {margin-top: 7rem;opacity: 1;}
+}
+
 iframe{
   border: 0;
   height: 20rem;
@@ -56,12 +76,9 @@ iframe{
 .header{
   display: flex;
   align-items: center;
+  justify-content: center;
   width: 100%;
   height: 20rem;
-  //background-image: url("../../assets/images/test.jpg");
-  background-repeat: no-repeat;
-  background-size: 100%;
-  background-position: 0 center;
   .title{
     font-size: 1.8rem;
     font-weight: bold;
@@ -69,14 +86,69 @@ iframe{
     left: 4rem;
   }
   .button-container{
+    animation: button-anim 1.5s ease 1.5s forwards;
+    -webkit-animation: button-anim 1.5s ease 1.5s forwards; /* Safari 与 Chrome */
     position: absolute;
-    right: 2rem;
+    justify-content: center;
+    margin-top: 7rem;
     display: flex;
+    font-size: 1.3rem;
+    opacity: 0;
     .iconfont{
-      margin-right: 0.2rem;
+      margin-right: 0.4rem;
+      font-size: 1.3rem;
     }
   }
 
+}
+.header-mobile{
+  opacity: 0;
+}
+@media screen and (max-width: 600px){
+
+  @keyframes mobile-header
+  {
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+  }
+  
+  @-webkit-keyframes mobile-header
+  {
+      0% {
+        background: #000;
+        height: 0rem;
+        opacity: 0;
+      }
+      100% {
+        background: #fff;
+        height: 3rem;
+        opacity: 1;
+      }
+  }
+
+  .button-container{
+    visibility: collapse;
+  }
+
+  .header-mobile{
+    
+    height: 3rem;
+    width: 100%;
+    position: absolute;
+    animation: mobile-header 1.5s ease forwards;
+    -webkit-animation: mobile-header 1.5s ease forwards; /* Safari 与 Chrome */
+    .header-bg{
+      opacity: 0;
+    }
+    .button-container-mobile{
+      margin-top: 1.1rem;
+      margin-left: 1.1rem;
+    }
+  }
 }
 .header-button{
   margin-left: 1.1rem;
