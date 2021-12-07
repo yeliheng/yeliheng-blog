f565fea331b67a1123f62455611a7490f5cbb254
Yeliheng <yeliheng00@163.com>
2021-12-08 00:15:39 +0800

    feat: 完成侧边栏内容

--- blog-vue/blog/src/views/home/Home.vue
+++ blog-vue/blog/src/views/home/Home.vue
@@ -1,18 +1,17 @@
 <template>
 <div class="home-container">
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
-  <div class="header-mobile">
-    <div class="header-bg">
+  <!-- 头部(移动端) -->
+    <div class="header-mobile">
+      <div class="button-container-mobile">
+          <span class="iconfont icon-mulu" style="font-size: 1.4rem;"></span>
+      </div>
     </div>
-    <div class="button-container-mobile">
-        <span class="iconfont icon-mulu" style="font-size: 1.4rem;"></span>
-    </div>
-    
-  </div>
+  <!-- 头部(PC) -->
     <div class="header">
         <!-- TODO: 后台获取站点信息 -->
-        <!-- <div class="title">Yeliheng的技术小站</div> -->
         <iframe src="fontmesh/index.html"></iframe>
+        
          <div class="button-container">
             <div class="index-button header-button">
                 <span class="iconfont icon-shouye"></span>
@@ -38,30 +37,57 @@
             </div>
         </div>
     </div>
+
+    <!-- 侧边栏(PC) -->
     <div class="sidebar">
+      <!-- 作者信息 -->
         <div class="my-info">
             <div class="avatar"></div>
-            <span>Yeliheng</span>
+            <span class="name">Yeliheng</span>
             <div class="art-info">
-              <div class="article-count">
+              <div class="article-count count">
                 <span>文章</span>
                 <span>12</span>
               </div>
-              <div class="category-count">
+              <div class="line"></div>
+              <div class="category-count count">
                 <span>分类</span>
                 <span>5</span>
               </div>
-              <div class="tag-count">
+              <div class="line"></div>
+              <div class="tag-count count">
                 <span>标签</span>
                 <span>20</span>
               </div>
             </div>
-            <span class="iconfont icon-github"></span>
-            <span class="iconfont icon-github"></span>
-            <span class="iconfont icon-github"></span>
+            <div class="contract-container">
+              <span class="iconfont icon-github"></span>
+              <span class="iconfont icon-Discord"></span>
+              <span class="iconfont icon-email-fill"></span>
+            </div>
+            
         </div>
-        <div class="site-info"></div>
+        <!-- 站点信息/公告 -->
+        <div class="site-info">
+            <span style="font-size: 1.3rem; font-weight: bold; display: flex; justify-content: center;margin-top: 1.3rem;">站点公告</span>
+            <span class="announcement">这是Yeliheng的个人博客,我会在这里分享我计算机学习生涯中的笔记、总结、技术干货...</span>
+            <div class="site-state">
+              <div>
+                <span class="iconfont icon-zhinanzhen"></span>
+                <span> 本站建立于2018年11月19日</span>
+              </div>
+              <div>
+                <span class="iconfont icon-wo"></span> 
+                <span> ©2022 Yeliheng 版权所有</span>
+              </div>
+                
+                <span>转载请注明出处!</span>
+            </div>
+
+        </div>
     </div>
+    
+    <!-- 内容区 -->
     <div class="content"></div>
 </div>
 
@@ -81,12 +107,28 @@ export default {
     0%   {margin-top: 2rem;opacity: 0;}
     100% {margin-top: 7rem;opacity: 1;}
 }
- 
-@-webkit-keyframes button-anim /* Safari 与 Chrome */
-{
-    0%   {margin-top: 2rem;opacity: 0;}
-    100% {margin-top: 7rem;opacity: 1;}
+
+@keyframes change-opcity{
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
 }
+
+@keyframes bottom-top-anim{
+    0% {
+      opacity: 0;
+      margin-top: 10rem;
+    }
+    100% {
+      opacity: 1;
+      margin-top: 0.5rem;
+    }
+}
+
+   
 
 iframe{
   border: 0;
@@ -106,13 +148,13 @@ iframe{
     left: 4rem;
   }
   .button-container{
+    color: #fff;
     animation: button-anim 1.5s ease 1.5s forwards;
-    -webkit-animation: button-anim 1.5s ease 1.5s forwards; /* Safari 与 Chrome */
     position: absolute;
     justify-content: center;
     margin-top: 7rem;
     display: flex;
-    font-size: 1.3rem;
+    font-size: 1.2rem;
     opacity: 0;
     .iconfont{
       margin-right: 0.4rem;
@@ -121,53 +163,125 @@ iframe{
   }
 
 }
+
+// 侧边栏
+.sidebar{
+  animation: bottom-top-anim 1s ease 1.5s forwards;
+  opacity: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 18rem;
+  margin: {
+    top: 0.5rem;
+    left: 0.5rem
+  };
+  // 我的信息
+  .my-info{
+    background: #121212;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .avatar{
+      margin-top: 1rem;
+      width: 7rem;
+      height: 7rem;
+      border-radius: 50%;
+      background-image: url("../../assets/images/avater.jpg");
+      background-repeat: round;
+    }
+    .name{
+      margin-top: 1rem;
+      font-size: 1.5rem;
+      font-weight: bold;
+    }
+    .art-info{
+      display: flex;
+      font-size: 1.2rem;
+      .line{
+        background: #353535;
+        margin-top: 2rem;
+        height: 3.5rem;
+        width: 0.05rem;
+      }
+      .count{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: {
+          left: 1rem;
+          right: 1rem;
+          top: 1.5rem;
+        };
+      }
+    }
+    .contract-container{
+      display: flex;        
+      flex-direction: row;
+      justify-content: center;
+      margin-top: 1.5rem;
+      .iconfont{
+        margin-left: 0.9rem;
+        margin-right: 0.9rem;
+        margin-bottom: 3rem;
+        font-size: 2rem;
+      }
+    }
+  }
+
+  // 站点信息
+  .site-info{
+    height: 15rem;
+    margin-top: 0.5rem;
+    background: #121212;
+    
+    .announcement{
+      display: block;
+      margin: 1.2rem;
+      text-align: center;
+    }
+    .site-state{
+      text-align: center;
+    
+    }
+  }
+}
+
 .header-mobile{
   opacity: 0;
+  .button-container-mobile{
+    display: none;
+  }
 }
+
+/* 移动端 */
 @media screen and (max-width: 600px){
 
-  @keyframes mobile-header
-  {
-      0% {
-        opacity: 0;
-      }
-      100% {
-        opacity: 1;
-      }
-  }
-  
-  @-webkit-keyframes mobile-header
-  {
-      0% {
-        background: #000;
-        height: 0rem;
-        opacity: 0;
-      }
-      100% {
-        background: #fff;
-        height: 3rem;
-        opacity: 1;
-      }
-  }
+
 
   .button-container{
     visibility: collapse;
   }
 
-  .header-mobile{
-    
+  .header-mobile{  
     height: 3rem;
     width: 100%;
     position: absolute;
-    animation: mobile-header 1.5s ease forwards;
-    -webkit-animation: mobile-header 1.5s ease forwards; /* Safari 与 Chrome */
+    animation: change-opcity 1.5s ease forwards;
     .header-bg{
       opacity: 0;
     }
     .button-container-mobile{
-      margin-top: 1.1rem;
-      margin-left: 1.1rem;
+      display: inline;
+      position: absolute;
+      top: 1.1rem;
+      left: 1.1rem;
     }
+  }
+
+  .sidebar{
+    display: none;
   }
 }
 .header-button{
@@ -176,5 +290,7 @@ iframe{
     cursor: pointer;
   }
 }
+
+
 
 </style>
