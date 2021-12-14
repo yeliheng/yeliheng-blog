a0d65a1a7959355d330591ef26aa2ae3c1ce0a63
yeliheng <yeliheng00@163.com>
2021-12-14 17:01:40 +0800

    feat: 为管理后台添加代码高亮

---
+++ blog-vue/admin/babel.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      'prismjs',
+      {
+        languages: ['java','json'],
+      },
+    ],
+  ],
+}
