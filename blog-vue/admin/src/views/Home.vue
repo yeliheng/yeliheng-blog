a073fb4f0030d96e348443b9b79c5d1d70103970
Akaneiny <nonerror87@gmail.com>
2022-05-22 21:08:41 +0800

    fix: fix home page layout

--- blog-vue/admin/src/views/Home.vue
+++ blog-vue/admin/src/views/Home.vue
@@ -1,46 +1,47 @@
 <template>
-    <div class="home-container">
-      <div class="card-container">
-        <div class="card-item">
-          <div class="card-title"><i class="fa fa-user" style="color: #409EFF"/> 总用户数</div>
-          <div class="split-line"></div>
-          <div class="primary-data">2,723</div>
-          <div class="compare-data">较昨日: <i class="fa fa-arrow-up" style="color: #52C41A"/>  193</div>
-        </div>
+  <div class="home-container">
+    <div class="card-container">
+      <div class="card-item" style="margin-left: 0;">
+        <div class="card-title"><i class="fa fa-user" style="color: #409EFF" /> 总用户数</div>
+        <div class="split-line"></div>
+        <div class="primary-data">2,723</div>
+        <div class="compare-data">较昨日: <i class="fa fa-arrow-up" style="color: #52C41A" /> 193</div>
+      </div>
 
-        <div class="card-item">
-          <div class="card-title"><i class="fa fa-laptop" style="color: #F5222D"/> 实时在线</div>
-          <div class="split-line"></div>
-          <div class="primary-data">1,024</div>
-          <div class="compare-data">
-            <i class="fa fa-mobile" style="color: #1890FF"/> 手机: 512
-            <i class="fa fa-desktop" style="color: #1890FF; margin-left: 0.5rem;"/> 电脑: 512</div>
-        </div>
-
-        <div class="card-item">
-          <div class="card-title"><i class="fa fa-dashboard" style="color: #41c596"/> 系统状态</div>
-          <div class="split-line"></div>
-          <div class="primary-data">正常</div>
-          <div class="compare-data"><i class="fa fa-warning" style="color: #FACC14"/> 无警告</div>
-        </div>
-
-        <div class="card-item">
-          <div class="card-title"><i class="fa fa-eye" style="color: #8543E0"/> 今日访问</div>
-          <div class="split-line"></div>
-          <div class="primary-data">9,798</div>
-          <div class="compare-data"><i  class="fa fa-futbol-o" style="color: #e322c7"/> 总访问: 153,234</div>
+      <div class="card-item">
+        <div class="card-title"><i class="fa fa-laptop" style="color: #F5222D" /> 实时在线</div>
+        <div class="split-line"></div>
+        <div class="primary-data">1,024</div>
+        <div class="compare-data">
+          <i class="fa fa-mobile" style="color: #1890FF" /> 手机: 512
+          <i class="fa fa-desktop" style="color: #1890FF; margin-left: 0.5rem;" /> 电脑: 512
         </div>
       </div>
-        <div class="chart-container" id="chart1">图表</div>
-        <div class="second-chart-container">
-          <div class="chart-item" id="chart2">
-            图表2
-          </div>
-          <div class="chart-item" id="chart3" style="margin-left: 1rem">
-            图表3
-          </div>
-        </div>
+
+      <div class="card-item">
+        <div class="card-title"><i class="fa fa-dashboard" style="color: #41c596" /> 系统状态</div>
+        <div class="split-line"></div>
+        <div class="primary-data">正常</div>
+        <div class="compare-data"><i class="fa fa-warning" style="color: #FACC14" /> 无警告</div>
+      </div>
+
+      <div class="card-item">
+        <div class="card-title"><i class="fa fa-eye" style="color: #8543E0" /> 今日访问</div>
+        <div class="split-line"></div>
+        <div class="primary-data">9,798</div>
+        <div class="compare-data"><i class="fa fa-futbol-o" style="color: #e322c7" /> 总访问: 153,234</div>
+      </div>
     </div>
+    <div class="chart-container" id="chart1">图表</div>
+    <div class="second-chart-container">
+      <div class="chart-item" id="chart2">
+        图表2
+      </div>
+      <div class="chart-item" id="chart3" style="margin-left: 1rem">
+        图表3
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -53,7 +54,8 @@ export default {
       let echart1 = echarts.init(document.getElementById("chart1"));
       const option = {
         title: {
-          text: '周统计'
+          text: '周统计',
+          padding: [20, 20, 20, 20]
         },
         tooltip: {
           trigger: 'axis'
@@ -134,7 +136,8 @@ export default {
 
       const option2 = {
         title: {
-          text: '五维分析'
+          text: '五维分析',
+          padding: [20, 20, 20, 20]
         },
         legend: {
           data: ['用户行为', '用户行为2']
@@ -173,7 +176,8 @@ export default {
 
       const option3 = {
         title: {
-          text: '用户来源'
+          text: '用户来源',
+          padding: [20, 20, 20, 20]
         },
         tooltip: {
           trigger: 'item'
@@ -224,77 +228,86 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .card-container {
-    display: flex;
-    width: 100%;
-    height: 14rem;
-    .card-item {
-      margin-left: 1rem;
-      background: #fff;
-      width: 25%;
-      height: 90%;
-      .card-title {
-        margin: {
-          left: 1.71rem;
-          top: 1.5rem;
-        }
-        color: rgba(0, 0, 0, 0.45);
-      }
-      .split-line {
-        height: 0.1rem;
-        background: #E8E8E8;
-        margin: 1rem;
-      }
-      .primary-data {
-        font-size: 2.14rem;
-        color: rgba(0, 0, 0, 0.6);
-        margin: {
-          left: 1.71rem;
-          top: 0.5rem;
-        }
-      }
+.card-container {
+  display: flex;
+  width: 100%;
+  height: 14rem;
 
-      .compare-data {
-        margin: {
-          left: 1.71rem;
-          top: 0.7rem;
-        }
-      }
-
-    }
-  }
-  .chart-container {
+  .card-item {
+    margin-left: 1rem;
     background: #fff;
-    width: 100%;
-    height: 28.86rem;
-  }
+    width: 25%;
+    height: 90%;
 
-  .second-chart-container {
-    display: flex;
-    height: 30rem;
-    width: 100%;
-    margin-top: 1rem;
-    .chart-item {
-      width: 100%;
-      background: #fff;
+    .card-title {
+      margin: {
+        left: 1.71rem;
+        top: 1.5rem;
+      }
+
+      color: rgba(0, 0, 0, 0.45);
     }
-  }
 
-  @media screen and (max-width: 900px) {
-    .card-container {
-      height: auto;
-      flex-direction: column;
-      .card-item {
-        width: 100%;
-        height: 13rem;
+    .split-line {
+      height: 0.1rem;
+      background: #E8E8E8;
+      margin: 1rem;
+    }
 
-        margin-left: 0rem;
-        margin-top: 1rem;
+    .primary-data {
+      font-size: 2.14rem;
+      color: rgba(0, 0, 0, 0.6);
+
+      margin: {
+        left: 1.71rem;
+        top: 0.5rem;
       }
     }
 
-    #chart1 {
+    .compare-data {
+      margin: {
+        left: 1.71rem;
+        top: 0.7rem;
+      }
+    }
+
+  }
+}
+
+.chart-container {
+  background: #fff;
+  width: 100%;
+  height: 28.86rem;
+}
+
+.second-chart-container {
+  display: flex;
+  height: 30rem;
+  width: 100%;
+  margin-top: 1rem;
+
+  .chart-item {
+    width: 100%;
+    background: #fff;
+  }
+}
+
+@media screen and (max-width: 900px) {
+  .card-container {
+    height: auto;
+    flex-direction: column;
+
+    .card-item {
+      width: 100%;
+      height: 13rem;
+
+      margin-left: 0rem;
       margin-top: 1rem;
     }
   }
+
+  #chart1 {
+    margin-top: 1rem;
+  }
+}
 </style>
